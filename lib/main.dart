fd37c2fc0eaf1347a35eab8db14fba57ba0c604a
Shi Jin <jinzishuai@gmail.com>
2023-03-13 20:09:06 -0600

    resolves #2:  import firebase_options.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:tiktok_tutorial/constants.dart';
 import 'package:tiktok_tutorial/controllers/auth_controller.dart';
 import 'package:tiktok_tutorial/views/screens/auth/login_screen.dart';
 import 'package:tiktok_tutorial/views/screens/auth/signup_screen.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((value) {
     Get.put(AuthController());
   });
   runApp(const MyApp());
